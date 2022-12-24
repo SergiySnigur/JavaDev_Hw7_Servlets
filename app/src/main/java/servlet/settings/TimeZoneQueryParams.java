dd3669e53779aa7fdb0cc28a5fa74a525cf8c6f2
SergiySnigur <serzhsnigur@gmail.com>
2022-12-24 18:56:18 +0200

    second commit

--- app/src/main/java/servlet/settings/TimeZoneQueryParams.java
+++ app/src/main/java/servlet/settings/TimeZoneQueryParams.java
@@ -1,14 +1,15 @@
 package servlet.settings;
 
-//import servlet.TimezoneValidateFilter;
-
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
 
 public class TimeZoneQueryParams {
     public String parseQueryParams(HttpServletRequest request) {
-        return request.getParameter("timezone")
-                .replace(" ", "+").toUpperCase();
+        if (request.getParameterMap().containsKey("timezone")) {
+            return (request.getParameter("timezone").replace(" ", "+").length() < 1) ?
+                    "UTC" : request.getParameter("timezone").replace(" ", "+").toUpperCase();
+        }
+        return "UTC";
     }
 }
